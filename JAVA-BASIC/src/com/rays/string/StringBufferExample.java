d0591bb6efde91061a46b2082fa231954fdd1d93
Ayushdabi <udaydabi7@gmail.com>
2024-10-17 13:02:31 +0530

    done

---
+++ JAVA-BASIC/src/com/rays/string/StringBufferExample.java
@@ -0,0 +1,23 @@
+package com.rays.string;
+
+public class StringBufferExample {
+
+	public static void main(String[] args) {
+
+		StringBuffer sb = new StringBuffer("Ram");
+		
+//		sb.insert(3, "n");
+//		
+//		sb.delete(0, 3);
+		
+//		System.out.println(sb);
+//
+//		sb.append(" Verma");
+		
+//		System.out.println(sb);
+
+		System.out.println(sb.reverse());
+
+	}
+
+}
